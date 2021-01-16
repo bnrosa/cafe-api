03729e1dc75e4b4113d7cb7161a83ecea2b0ff1b
Bernardo Rosa <bernardoorosa@gmail.com>
2021-01-16 15:02:50 -0300

    :construction: Added GraphQL types

---
+++ typeDefs/types/ItemType.js
@@ -0,0 +1,28 @@
+const {
+  GraphQLID,
+  GraphQLString,
+  GraphQLObjectType,
+  GraphQLInt,
+} = require("graphql");
+
+//Import models
+const Category = require("../models/Category");
+
+//Defining type
+const ItemType = new GraphQLObjectType({
+  name: "Item",
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    price: { type: GraphQLInt },
+    photo: { type: GraphQLString },
+    category: {
+      type: CategoryType,
+      resolve(parent, args) {
+        return Category.findById(parent.authorId);
+      },
+    },
+  }),
+});
+
+module.exports = ItemType;
